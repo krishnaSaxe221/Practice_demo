8afd161d44b95c9ceaa580c8cc1af34362ce2fdf
krishna saxena <100754752+krishnaSaxe221@users.noreply.github.com>
2025-01-26 22:39:42 +0530

    Added Add two numbers

---
+++ AddTwoNumbers.java
@@ -0,0 +1,10 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first no");
+        int num1 = sc.nextInt();
+        System.out.println("Enter second number");
+        int num2 = sc.nextInt();
+        System.out.println(num1 + num2);
+    }
+}
